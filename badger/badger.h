a12dc41e24740b87cfe8b6889394d36fbf434d98
aligorkem <aligorkem@localhost>
2016-06-15 12:38:49 +1000

    Initial Commit

---
+++ badger/badger.h
@@ -0,0 +1,19 @@
+//
+//  badger.h
+//  badger
+//
+//  Created by aligorkem on 15/06/2016.
+//  Copyright © 2016 aligorkem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for badger.
+FOUNDATION_EXPORT double badgerVersionNumber;
+
+//! Project version string for badger.
+FOUNDATION_EXPORT const unsigned char badgerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <badger/PublicHeader.h>
+
+
